850a5e7c8f6ea8063147bd96f0e170d6effdaa9e
alex <sasha-bill@ya.ru>
2020-02-18 23:57:59 +0300

    cors, requirements and minor fix

--- django_vue_simpletone/settings.py
+++ django_vue_simpletone/settings.py
@@ -39,18 +39,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'notes'
+    'notes',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'django_vue_simpletone.urls'
 
